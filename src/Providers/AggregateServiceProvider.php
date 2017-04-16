e1686460e50c673d37739a38cf5fd7d0f9e940a0
Abdelrahman Omran <me@omranic.com>
2017-04-17 00:45:58 +0200

    Remove file header docblock

--- src/Providers/AggregateServiceProvider.php
+++ src/Providers/AggregateServiceProvider.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
- * NOTICE OF LICENSE
- *
- * Part of the Cortex Fort Module.
- *
- * This source file is subject to The MIT License (MIT)
- * that is bundled with this package in the LICENSE file.
- *
- * Package: Cortex Fort Module
- * License: The MIT License (MIT)
- * Link:    https://rinvex.com
- */
-
 declare(strict_types=1);
 
 namespace Cortex\Fort\Providers;
