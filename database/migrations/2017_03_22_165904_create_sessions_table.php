facd94817f6744e2ec69b875adb59bedcf2b0433
Abdelrahman Omran <me@omranic.com>
2017-04-29 22:40:05 +0200

    Move sessions migration to cortex/foundation module

---
+++ database/migrations/2017_03_22_165904_create_sessions_table.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('sessions', function (Blueprint $table) {
+            // Columns
+            $table->string('id')->unique();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity');
+
+            // Indexes
+            $table->foreign('user_id')->references('id')->on(config('rinvex.fort.tables.users'))
+                  ->onDelete('cascade')->onUpdate('cascade');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::dropIfExists('sessions');
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
