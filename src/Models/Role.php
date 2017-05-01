569ded034795fbe4891f94e07b5d3e848cc71f52
Abdelrahman Omran <me@omranic.com>
2017-05-01 02:49:00 +0200

    Override default models

---
+++ src/Models/Role.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Fort\Models;
+
+use Rinvex\Fort\Models\Role as BaseRole;
+use Rinvex\Attributable\Traits\Attributable;
+
+/**
+ * Cortex\Fort\Models\Role
+ *
+ * @property int                                                                         $id
+ * @property string                                                                      $slug
+ * @property string                                                                      $name
+ * @property string                                                                      $description
+ * @property \Carbon\Carbon                                                              $created_at
+ * @property \Carbon\Carbon                                                              $updated_at
+ * @property \Carbon\Carbon                                                              $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Rinvex\Fort\Models\Ability[] $abilities
+ * @property-read array                                                                  $ability_list
+ * @property mixed                                                                       $entity
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Cortex\Fort\Models\User[]    $users
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role hasAttribute($key, $value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role whereDeletedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role whereSlug($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\Role whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Role extends BaseRole
+{
+    use Attributable;
+}
---
+++ src/Models/User.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Fort\Models;
+
+use Rinvex\Fort\Models\User as BaseUser;
+use Rinvex\Attributable\Traits\Attributable;
+
+/**
+ * Cortex\Fort\Models\User
+ *
+ * @property int                                                                                                            $id
+ * @property string                                                                                                         $username
+ * @property string                                                                                                         $password
+ * @property string                                                                                                         $remember_token
+ * @property string                                                                                                         $email
+ * @property bool                                                                                                           $email_verified
+ * @property \Carbon\Carbon                                                                                                 $email_verified_at
+ * @property string                                                                                                         $phone
+ * @property bool                                                                                                           $phone_verified
+ * @property \Carbon\Carbon                                                                                                 $phone_verified_at
+ * @property string                                                                                                         $name_prefix
+ * @property string                                                                                                         $first_name
+ * @property string                                                                                                         $middle_name
+ * @property string                                                                                                         $last_name
+ * @property string                                                                                                         $suffix
+ * @property string                                                                                                         $job_title
+ * @property string                                                                                                         $country_code
+ * @property string                                                                                                         $language_code
+ * @property array                                                                                                          $two_factor
+ * @property \Carbon\Carbon                                                                                                 $birthdate
+ * @property string                                                                                                         $gender
+ * @property bool                                                                                                           $active
+ * @property \Carbon\Carbon                                                                                                 $login_at
+ * @property \Carbon\Carbon                                                                                                 $created_at
+ * @property \Carbon\Carbon                                                                                                 $updated_at
+ * @property \Carbon\Carbon                                                                                                 $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Rinvex\Fort\Models\Ability[]                                    $abilities
+ * @property-read array                                                                                                     $ability_list
+ * @property-read \Illuminate\Support\Collection                                                                            $all_abilities
+ * @property-read \Rinvex\Country\Country                                                                                   $country
+ * @property mixed                                                                                                          $entity
+ * @property-read \Rinvex\Language\Language                                                                                 $language
+ * @property-read string                                                                                                    $name
+ * @property-read array                                                                                                     $role_list
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Rinvex\Fort\Models\Persistence[]                                $persistences
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Cortex\Fort\Models\Role[]                                       $roles
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Rinvex\Fort\Models\Socialite[]                                  $socialites
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User hasAttribute($key, $value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User role($roles)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereActive($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereBirthdate($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereCountryCode($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLanguageCode($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereEmailVerified($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereFirstName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereGender($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereJobTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLastName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLoginAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereMiddleName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePhone($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePhoneVerified($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePhoneVerifiedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereNamePrefix($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereSuffix($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereTwoFactor($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereUsername($value)
+ * @mixin \Eloquent
+ */
+class User extends BaseUser
+{
+    use Attributable;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $with = ['eav'];
+}
