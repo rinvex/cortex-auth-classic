6d302e7f147b3a9f82f54a73db4aa7cf925d1e7b
Abdelrahman Omran <me@omranic.com>
2017-05-02 14:08:09 +0200

    Update user model docblock

--- src/Models/User.php
+++ src/Models/User.php
@@ -24,13 +24,19 @@ use Rinvex\Attributable\Traits\Attributable;
  * @property string                                                                                                         $first_name
  * @property string                                                                                                         $middle_name
  * @property string                                                                                                         $last_name
- * @property string                                                                                                         $suffix
+ * @property string                                                                                                         $name_suffix
  * @property string                                                                                                         $job_title
  * @property string                                                                                                         $country_code
  * @property string                                                                                                         $language_code
  * @property array                                                                                                          $two_factor
- * @property \Carbon\Carbon                                                                                                 $birthdate
+ * @property \Carbon\Carbon                                                                                                 $birthday
  * @property string                                                                                                         $gender
+ * @property string                                                                                                         $website
+ * @property string                                                                                                         $twitter
+ * @property string                                                                                                         $facebook
+ * @property string                                                                                                         $linkedin
+ * @property string                                                                                                         $google_plus
+ * @property string                                                                                                         $skype
  * @property bool                                                                                                           $active
  * @property \Carbon\Carbon                                                                                                 $login_at
  * @property \Carbon\Carbon                                                                                                 $created_at
@@ -52,31 +58,37 @@ use Rinvex\Attributable\Traits\Attributable;
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User hasAttribute($key, $value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User role($roles)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereActive($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereBirthdate($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereBirthday($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereCountryCode($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLanguageCode($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereCreatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereDeletedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereEmail($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereEmailVerified($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereFacebook($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereFirstName($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereGender($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereGooglePlus($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereId($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereJobTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLanguageCode($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLastName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLinkedin($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereLoginAt($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereMiddleName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereNamePrefix($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereNameSuffix($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePassword($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePhone($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePhoneVerified($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User wherePhoneVerifiedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereNamePrefix($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereRememberToken($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereSuffix($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereSkype($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereTwitter($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereTwoFactor($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereUpdatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereUsername($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Fort\Models\User whereWebsite($value)
  * @mixin \Eloquent
  */
 class User extends BaseUser
