958c170b08902fa964443b9fa73d4a3f5c21d478
Abdelrahman Omran <me@omranic.com>
2017-06-12 00:00:22 +0200

    Enforce consistency

--- resources/views/frontend/authentication/register.blade.php
+++ resources/views/frontend/authentication/register.blade.php
@@ -28,18 +28,16 @@
         </div>
         <!-- /.register-logo -->
         <div class="register-box-body">
-            <p class="register-box-msg">{{ trans('cortex/fort::common.register') }}</p>
+            <p class="register-box-msg">{{ trans('cortex/fort::common.register_new_account') }}</p>
 
             {{ Form::open(['url' => route('frontend.auth.register.process')]) }}
 
             <div class="form-group has-feedback{{ $errors->has('username') ? ' has-error' : '' }}">
                 {{ Form::text('username', old('username'), ['class' => 'form-control', 'placeholder' => trans('cortex/fort::common.username'), 'required' => 'required', 'autofocus' => 'autofocus']) }}
-                <span class="fa fa-envelope form-control-feedback"></span>
+                <span class="fa fa-font form-control-feedback"></span>
 
                 @if ($errors->has('username'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('username') }}</strong>
-                    </span>
+                    <span class="help-block">{{ $errors->first('username') }}</span>
                 @endif
             </div>
 
@@ -48,37 +46,31 @@
                 <span class="fa fa-envelope form-control-feedback"></span>
 
                 @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
+                    <span class="help-block">{{ $errors->first('email') }}</span>
                 @endif
             </div>
 
             <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
                 {{ Form::password('password', ['class' => 'form-control', 'placeholder' => trans('cortex/fort::common.password'), 'required' => 'required']) }}
-                <span class="fa fa-lock form-control-feedback"></span>
+                <span class="fa fa-key form-control-feedback"></span>
 
                 @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
+                    <span class="help-block">{{ $errors->first('password') }}</span>
                 @endif
             </div>
 
             <div class="form-group has-feedback{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                 {{ Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => trans('cortex/fort::common.password_confirmation'), 'required' => 'required']) }}
-                <span class="fa fa-lock form-control-feedback"></span>
+                <span class="fa fa-key form-control-feedback"></span>
 
                 @if ($errors->has('password_confirmation'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                    </span>
+                    <span class="help-block">{{ $errors->first('password_confirmation') }}</span>
                 @endif
             </div>
 
             <div class="row">
                 <div class="col-xs-8">
-                    {{ Html::link(route('frontend.auth.login'), trans('cortex/fort::common.registerd_already')) }}
+                    {{ Html::link(route('frontend.auth.login'), trans('cortex/fort::common.registered_already')) }}
 
                     {{--<div class="checkbox icheck">--}}
                     {{--<label for="terms_agreement">--}}
