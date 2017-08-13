0ec56238f674215ee26cc54b2571c58e9e4b630a
Abdelrahman Omran <me@omranic.com>
2017-08-13 03:29:01 +0200

    Refactor menus

---
+++ routes/menus.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Spatie\Menu\Laravel\Link;
+use Spatie\Menu\Laravel\Html;
+use Cortex\Foundation\Models\Menu as MenuModel;
+
+Menu::backendSidebar('resources')->routeIfCan('list-abilities', 'backend.abilities.index', '<i class="fa fa-sliders"></i> <span>'.trans('cortex/fort::common.abilities').'</span>');
+Menu::backendSidebar('resources')->routeIfCan('list-roles', 'backend.roles.index', '<i class="fa fa-users"></i> <span>'.trans('cortex/fort::common.roles').'</span>');
+Menu::backendSidebar('resources')->routeIfCan('list-users', 'backend.users.index', '<i class="fa fa-user"></i> <span>'.trans('cortex/fort::common.users').'</span>');
+
+if ($user = auth()->user()) {
+    $userMenuHeader = Link::to('#', $user->username.' <span class="caret"></span>')->addClass('dropdown-toggle')->setAttribute('data-toggle', 'dropdown');
+    $userMenuBody = function (MenuModel $menu) {
+        $menu->addClass('dropdown-menu');
+        $menu->addParentClass('dropdown');
+        $menu->route('userarea.account.settings', '<i class="fa fa-user"></i> '.trans('cortex/fort::common.settings'));
+        $menu->route('userarea.account.sessions', '<i class="fa fa-id-badge"></i> '.trans('cortex/fort::common.sessions'));
+        $menu->add(Html::raw('')->addParentClass('divider')->setParentAttribute('role', 'separator'));
+
+        $logoutLink = Link::toRoute('frontend.auth.logout', '<i class="fa fa-sign-out"></i> '.trans('cortex/fort::common.logout'))->setAttribute('onclick', "event.preventDefault(); document.getElementById('logout-form').submit();");
+        $menu->add(Html::raw($logoutLink.Form::open(['url' => route('frontend.auth.logout'), 'id' => 'logout-form', 'style' => 'display: none;']).Form::close()));
+    };
+
+    Menu::userareaTopbar()->submenu($userMenuHeader, $userMenuBody);
+    Menu::frontendTopbar()->submenu($userMenuHeader, $userMenuBody);
+    Menu::backendTopbar()->submenu($userMenuHeader, $userMenuBody);
+} else {
+    Menu::frontendTopbar()->route('frontend.auth.login', trans('cortex/fort::common.login'));
+    Menu::frontendTopbar()->route('frontend.auth.register', trans('cortex/fort::common.register'));
+}
--- src/Providers/FortServiceProvider.php
+++ src/Providers/FortServiceProvider.php
@@ -6,7 +6,9 @@ namespace Cortex\Fort\Providers;
 
 use Cortex\Fort\Models\Role;
 use Cortex\Fort\Models\User;
+use Cortex\Fort\Models\Ability;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Compilers\BladeCompiler;
 
 class FortServiceProvider extends ServiceProvider
 {
@@ -27,28 +29,9 @@ class FortServiceProvider extends ServiceProvider
         // Publish Resources
         ! $this->app->runningInConsole() || $this->publishResources();
 
-        // Register sidebar menus
-        $this->app->singleton('menus.sidebar.access', function ($app) {
-            return collect();
-        });
-
-        // Register menu items
-        $this->app['view']->composer('cortex/foundation::backend.partials.sidebar', function ($view) {
-            app('menus.sidebar')->put('access', app('menus.sidebar.access'));
-            app('menus.sidebar.access')->put('header', '<li class="header">'.trans('cortex/fort::navigation.headers.access').'</li>');
-            app('menus.sidebar.access')->put('abilities', '<li '.(mb_strpos(request()->route()->getName(), 'backend.abilities.') === 0 ? 'class="active"' : '').'><a href="'.route('backend.abilities.index').'"><i class="fa fa-sliders"></i> <span>'.trans('cortex/fort::navigation.menus.abilities').'</span></a></li>');
-            app('menus.sidebar.access')->put('roles', '<li '.(mb_strpos(request()->route()->getName(), 'backend.roles.') === 0 ? 'class="active"' : '').'><a href="'.route('backend.roles.index').'"><i class="fa fa-users"></i> <span>'.trans('cortex/fort::navigation.menus.roles').'</span></a></li>');
-            app('menus.sidebar.access')->put('users', '<li '.(mb_strpos(request()->route()->getName(), 'backend.users.') === 0 ? 'class="active"' : '').'><a href="'.route('backend.users.index').'"><i class="fa fa-user"></i> <span>'.trans('cortex/fort::navigation.menus.users').'</span></a></li>');
-        });
-
         // Register attributable entities
         app('rinvex.attributable.entities')->push(Role::class);
         app('rinvex.attributable.entities')->push(User::class);
-
-        // Register menu items
-        $this->app['view']->composer('*.partials.header', function ($view) {
-            app('menus.topbar')->put('user', view('cortex/fort::frontend.partials.topbar-user-menu')->render());
-        });
     }
 
     /**
