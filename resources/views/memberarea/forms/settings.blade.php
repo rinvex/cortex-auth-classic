af044effd7ff999bab20ee807b6337f591d89b97
Marcus Reinhardt <webstone@gmail.com>
2017-09-25 22:10:44 +0200

    Update settings.blade.php

--- resources/views/memberarea/forms/settings.blade.php
+++ resources/views/memberarea/forms/settings.blade.php
@@ -56,9 +56,8 @@
                 <div class="col-md-6 col-md-offset-3">
                     <div class="nav-tabs-custom">
                         <ul class="nav nav-tabs">
-                            <li class="active"><a href="#basic-tab" data-toggle="tab">{{ trans('cortex/fort::common.basic_info') }}</a></li>
-                            <li><a href="#basic-tab" data-toggle="tab">{{ trans('cortex/fort::common.basic_info') }}</a></li>
-                            <li><a href="#basic-tab" data-toggle="tab">{{ trans('cortex/fort::common.basic_info') }}</a></li>
+                            <li class="active"><a href="{{ route('memberarea.account.settings') }}">{{ trans('cortex/fort::common.basic_info') }}</a></li>
+                            <li><a href="{{ route('memberarea.account.password') }}">{{ trans('cortex/fort::common.password') }}</a></li>
                         </ul>
 
                         <div class="tab-content">
@@ -247,37 +246,6 @@
                                             @endif
                                         </div>
                                     </div>
-
-
-                                    <hr />
-
-
-                                    <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-                                        {{ Form::label('password', trans('cortex/fort::common.password'), ['class' => 'col-md-2 control-label']) }}
-
-                                        <div class="col-md-10">
-                                            {{ Form::password('password', ['class' => 'form-control', 'placeholder' => trans('cortex/fort::common.password')]) }}
-                                            <span class="fa fa-key form-control-feedback"></span>
-
-                                            @if ($errors->has('password'))
-                                                <span class="help-block">{{ $errors->first('password') }}</span>
-                                            @endif
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group has-feedback{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                                        {{ Form::label('password_confirmation', trans('cortex/fort::common.password_confirmation'), ['class' => 'col-md-2 control-label']) }}
-
-                                        <div class="col-md-10">
-                                            {{ Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => trans('cortex/fort::common.password_confirmation')]) }}
-                                            <span class="fa fa-key form-control-feedback"></span>
-
-                                            @if ($errors->has('password_confirmation'))
-                                                <span class="help-block">{{ $errors->first('password_confirmation') }}</span>
-                                            @endif
-                                        </div>
-                                    </div>
-
 
                                     @if(! empty(config('rinvex.fort.twofactor.providers')))
 
