24b4b45f6cebe4854e8c488cd2706ed035359d90
Abdelrahman Omran <me@omranic.com>
2017-09-28 23:37:11 +0200

    Extend core rivnex/fort SeedCommand

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace Cortex\Fort\Console\Commands;
 
-use Carbon\Carbon;
-use Illuminate\Console\Command;
-use Rinvex\Support\Traits\SeederHelper;
+use Rinvex\Fort\Console\Commands\SeedCommand as BaseSeedCommand;
 
-class SeedCommand extends Command
+class SeedCommand extends BaseSeedCommand
 {
-    use SeederHelper;
-
     /**
      * The name and signature of the console command.
      *
@@ -33,45 +29,8 @@ class SeedCommand extends Command
      */
     public function handle()
     {
-        $this->warn('Seed cortex/fort:');
+        parent::handle();
 
-        if ($this->ensureExistingDatabaseTables('rinvex/fort')) {
-            $this->seedResources(app('rinvex.fort.ability'), realpath(__DIR__.'/../../../resources/data/abilities.json'), ['name', 'description', 'policy']);
-            $this->seedResources(app('rinvex.fort.role'), realpath(__DIR__.'/../../../resources/data/roles.json'), ['name', 'description'], function () {
-                // Grant abilities to roles
-                app('rinvex.fort.role')->where('slug', 'operator')->first()->grantAbilities('superadmin', 'global');
-            });
-            $this->seedUsers();
-        }
-    }
-
-    /**
-     * Seed default users.
-     *
-     * @return void
-     */
-    protected function seedUsers()
-    {
-        $this->warn('Seeding Users:');
-
-        $user = [
-            'username' => 'Cortex',
-            'email' => 'help@rinvex.com',
-            'email_verified' => true,
-            'is_active' => true,
-        ];
-
-        $user = tap(app('rinvex.fort.user')->firstOrNew($user)->fill([
-            'email_verified_at' => Carbon::now(),
-            'remember_token' => str_random(10),
-            'password' => $password = str_random(),
-        ]), function ($instance) {
-            $instance->save();
-        });
-
-        // Assign roles to users
-        $user->assignRoles('operator');
-
-        $this->table(['Username', 'Password'], [['username' => $user['username'], 'password' => $password]]);
+        $this->seedResources(app('rinvex.fort.role'), realpath(__DIR__.'/../../../resources/data/roles.json'), ['name', 'description']);
     }
 }
