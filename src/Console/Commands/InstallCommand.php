6fe8f48f5f795ce4f180cb00ff09059d731643de
Abdelrahman Omran <me@omranic.com>
2017-10-29 05:24:54 +0200

    Tweak artisan command output lines

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -29,7 +29,7 @@ class InstallCommand extends Command
      */
     public function handle()
     {
-        $this->warn('Install cortex/fort:');
+        $this->warn($this->description);
         $this->call('cortex:migrate:fort');
         $this->call('cortex:seed:fort');
         $this->call('cortex:publish:fort');
