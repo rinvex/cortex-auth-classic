be226f392f7050076b903c09bc31b8f707671dd6
Abdelrahman Omran <me@omranic.com>
2017-11-22 16:17:55 +0200

    Move limit menu to button strip

--- resources/views/tenantarea/pages/user-activities.blade.php
+++ resources/views/tenantarea/pages/user-activities.blade.php
@@ -5,5 +5,4 @@
     <li><a href="{{ route("tenantarea.{$type}.edit", [str_singular($type) => $resource]) }}">{{ trans('cortex/foundation::common.details') }}</a></li>
     <li><a href="{{ route("tenantarea.{$type}.logs", [str_singular($type) => $resource]) }}">{{ trans('cortex/foundation::common.logs') }}</a></li>
     <li class="active"><a href="#{{ $tab ?? 'activities' }}-tab" data-toggle="tab">{{ trans('cortex/foundation::common.activities') }}</a></li>
-    <li style="float: right; padding: 5px"><select class="form-control dataTableBuilderLengthChanger" aria-controls="{{ $id }}-table"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></li>
 @stop
--- resources/views/tenantarea/pages/user-logs.blade.php
+++ resources/views/tenantarea/pages/user-logs.blade.php
@@ -5,5 +5,4 @@
     <li><a href="{{ route("tenantarea.{$type}.edit", [str_singular($type) => $resource]) }}">{{ trans('cortex/foundation::common.details') }}</a></li>
     <li class="active"><a href="#{{ $tab }}-tab" data-toggle="tab">{{ trans('cortex/foundation::common.'.$tab) }}</a></li>
     @if(method_exists($resource, 'causedActivity')) <li><a href="{{ route("tenantarea.{$type}.activities", [str_singular($type) => $resource]) }}">{{ trans('cortex/foundation::common.activities') }}</a></li> @endif
-    <li style="float: right; padding: 5px"><select class="form-control dataTableBuilderLengthChanger" aria-controls="{{ $id }}-table"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></li>
 @stop
