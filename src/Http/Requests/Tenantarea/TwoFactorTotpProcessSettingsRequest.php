0c443402f5d8729afb4507b84643113ee4e70b16
Abdelrahman Omran <me@omranic.com>
2018-01-23 21:50:53 +0200

    Require six digits for TwoFactor TOTP token

--- src/Http/Requests/Tenantarea/TwoFactorTotpProcessSettingsRequest.php
+++ src/Http/Requests/Tenantarea/TwoFactorTotpProcessSettingsRequest.php
@@ -9,12 +9,26 @@ use Illuminate\Foundation\Http\FormRequest;
 class TwoFactorTotpProcessSettingsRequest extends FormRequest
 {
     /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @throws \Rinvex\Fort\Exceptions\GenericException
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
     public function rules(): array
     {
-        return ['token' => 'required|integer'];
+        return [
+            'token' => 'required|digits:6',
+        ];
     }
 }
