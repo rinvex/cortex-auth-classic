5cdcae370101050dc8743b65d4584295538d0ab7
Abdelrahman Omran <me@omranic.com>
2018-01-31 16:52:45 +0200

    Clean & simplify Reauthenticate middleware

--- src/Http/Middleware/Reauthenticate.php
+++ src/Http/Middleware/Reauthenticate.php
@@ -11,39 +11,28 @@ class Reauthenticate
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request  $request
-     * @param \Closure                  $next
-     * @param string                    $type
-     * @param string|null               $session_name
-     * @param int|null                  $timeout
-     * @param bool                      $renew
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param string                   $type
+     * @param int                      $timeout
+     * @param bool                     $renew
      *
      * @return mixed
      */
-    public function handle($request, Closure $next, $type='password', $session_name=null, $timeout=null, $renew=false)
+    public function handle($request, Closure $next, $type = 'password', $timeout = null, $renew = false)
     {
-
         $timeout = $timeout ?? config('cortex.fort.reauthentication.timeout');
-
-        if( is_null($session_name) || empty($session_name) ) {
-            $session_name = 'cortex.fort.reauthentication.'.$request->route()->getName();
-        } else {
-            $session_name = 'cortex.fort.reauthentication.'.$session_name;
-        }
-
-        if( is_null( session( $session_name ) ) || time() - session( $session_name ) >= $timeout ) {
-
-            session()->forget( $session_name );
-            session()->put('rinvex.fort.twofactor.totp', true);
+        $session_name = 'cortex.fort.reauthentication.'.$request->route()->getName();
+       
+        if(is_null(session($session_name)) || time() - session($session_name) >= $timeout) {
+            session()->forget($session_name);
             session()->put('cortex.fort.reauthentication.intended', $request->url());
             session()->put('cortex.fort.reauthentication.session_name', $session_name);
 
             return view('cortex/fort::frontarea.common.reauthentication.'.$type);
         }
 
-        if( $renew ) {
-            session()->put($session_name, time());
-        }
+        ! $renew || session()->put($session_name, time());
 
         return $next($request);
     }
