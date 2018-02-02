25702f8e76b9f04aeb621573373c5251ed58f432
Abdelrahman Omran <me@omranic.com>
2018-02-02 20:31:39 +0200

    Clean ReauthenticationController

--- src/Http/Controllers/Frontarea/ReauthenticationController.php
+++ src/Http/Controllers/Frontarea/ReauthenticationController.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Cortex\Fort\Http\Controllers\Frontarea;
 
-use Cortex\Fort\Traits\TwoFactorAuthenticatesUsers;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Cortex\Fort\Traits\TwoFactorAuthenticatesUsers;
 use Cortex\Foundation\Http\Controllers\AuthenticatedController;
 
 class ReauthenticationController extends AuthenticatedController
 {
-
     use TwoFactorAuthenticatesUsers;
 
     /**
@@ -19,24 +18,25 @@ class ReauthenticationController extends AuthenticatedController
      *
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
      */
-    public function processPassword( Request $request ) {
-
-        $session_name = session('cortex.fort.reauthentication.session_name');
+    public function processPassword(Request $request)
+    {
         $redirect_url = session('cortex.fort.reauthentication.intended');
+        $session_name = session('cortex.fort.reauthentication.session_name');
 
-        if( Hash::check($request->input('password'), request()->user()->password) ) {
+        if (Hash::check($request->input('password'), request()->user()->password)) {
             $this->setSession($session_name);
 
             return intend([
-                'intended' => url($redirect_url)
-            ]);
-        } else {
-            return intend([
                 'intended' => url($redirect_url),
-                'withErrors' => ['password' => trans('cortex/fort::messages.auth.failed')
-                ]
             ]);
         }
+
+        return intend([
+            'intended' => url($redirect_url),
+            'withErrors' => [
+                'password' => trans('cortex/fort::messages.auth.failed'),
+            ],
+        ]);
     }
 
     /**
@@ -44,37 +44,36 @@ class ReauthenticationController extends AuthenticatedController
      *
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
      */
-    public function processTwofactor( Request $request ) {
-
-        $session_name = session('cortex.fort.reauthentication.session_name');
+    public function processTwofactor(Request $request)
+    {
         $redirect_url = session('cortex.fort.reauthentication.intended');
+        $session_name = session('cortex.fort.reauthentication.session_name');
 
         $guard = $this->getGuard();
-        $token = $request->input('token');
         $user = $request->user($guard);
+        $token = (int) $request->input('token');
 
-        if( $this->attemptTwoFactor($user, $token) ) {
+        if ($this->attemptTwoFactor($user, $token)) {
             $this->setSession($session_name);
 
             return intend([
-                'intended' => url($redirect_url)
-            ]);
-        } else {
-            return intend([
                 'intended' => url($redirect_url),
-                'withErrors' => ['token' => trans('cortex/fort::messages.verification.twofactor.invalid_token')],
             ]);
         }
+
+        return intend([
+            'intended' => url($redirect_url),
+            'withErrors' => ['token' => trans('cortex/fort::messages.verification.twofactor.invalid_token')],
+        ]);
     }
 
     /**
      * @param $session_name
      */
-    protected  function setSession($session_name) {
+    protected function setSession($session_name)
+    {
         session()->put($session_name, time());
-        session()->forget('cortex.fort.reauthentication.session_name');
         session()->forget('cortex.fort.reauthentication.intended');
-        session()->forget('rinvex.fort.twofactor.totp');
+        session()->forget('cortex.fort.reauthentication.session_name');
     }
-
 }
