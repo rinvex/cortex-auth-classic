05c30f28c1e2baa8e20c5b43f214d3ed3fbb441b
Abdelrahman Omran <me@omranic.com>
2018-02-19 20:08:20 +0200

    Drop useless guardian form request logic

--- src/Http/Requests/Adminarea/GuardianFormRequest.php
+++ src/Http/Requests/Adminarea/GuardianFormRequest.php
@@ -19,62 +19,6 @@ class GuardianFormRequest extends FormRequest
     }
 
     /**
-     * Prepare the data for validation.
-     *
-     * @return void
-     */
-    protected function prepareForValidation(): void
-    {
-        $data = $this->all();
-
-        $guardian = $this->route('guardian') ?? app('cortex.auth.guardian');
-        $country = $data['country_code'] ?? null;
-        $twoFactor = $guardian->getTwoFactor();
-
-        $data['email_verified'] = $this->get('email_verified', false);
-        $data['phone_verified'] = $this->get('phone_verified', false);
-
-        if ($guardian->exists && empty($data['password'])) {
-            unset($data['password'], $data['password_confirmation']);
-        }
-
-        // Update email verification date
-        if ($data['email_verified'] && $guardian->email_verified !== $data['email_verified']) {
-            $data['email_verified_at'] = now();
-        }
-
-        // Update phone verification date
-        if ($data['phone_verified'] && $guardian->phone_verified !== $data['phone_verified']) {
-            $data['phone_verified_at'] = now();
-        }
-
-        // Set abilities
-        if ($this->user($this->get('guard'))->can('grant', \Cortex\Auth\Models\Ability::class)) {
-            $abilities = array_map('intval', $this->get('abilities', []));
-            $data['abilities'] = $this->user($this->get('guard'))->can('superadmin') ? $abilities
-                : $this->user($this->get('guard'))->abilities->pluck('id')->intersect($abilities)->toArray();
-        } else {
-            unset($data['abilities']);
-        }
-
-        // Set roles
-        if ($this->user($this->get('guard'))->can('assign', \Cortex\Auth\Models\Role::class) && $data['roles']) {
-            $roles = array_map('intval', $this->get('roles', []));
-            $data['roles'] = $this->user($this->get('guard'))->can('superadmin') ? $roles
-                : $this->user($this->get('guard'))->roles->pluck('id')->intersect($roles)->toArray();
-        } else {
-            unset($data['roles']);
-        }
-
-        if ($twoFactor && (isset($data['phone_verified_at']) || $country !== $guardian->country_code)) {
-            array_set($twoFactor, 'phone.enabled', false);
-            $data['two_factor'] = $twoFactor;
-        }
-
-        $this->replace($data);
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -85,8 +29,6 @@ class GuardianFormRequest extends FormRequest
         $guardian->updateRulesUniques();
         $rules = $guardian->getRules();
 
-        $rules['roles'] = 'nullable|array';
-        $rules['abilities'] = 'nullable|array';
         $rules['password'] = $guardian->exists
             ? 'confirmed|min:'.config('cortex.auth.password_min_chars')
             : 'required|confirmed|min:'.config('cortex.auth.password_min_chars');
