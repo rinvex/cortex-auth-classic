bf0c838cac614f6afa3bb3d23c95fbe42803d706
Abdelrahman Omran <me@omranic.com>
2018-02-26 17:20:58 +0200

    Fix wrong tenant registration language phrase

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -39,6 +39,8 @@ return [
     'entity_id' => 'Entity Id',
     'entity_type' => 'Entity Type',
     'only_owned' => 'Only Owned',
+    'owner_account' => 'Owner Account',
+    'tenant_details' => 'Tenant Details',
 
     // Fields
     'name' => 'Name',
