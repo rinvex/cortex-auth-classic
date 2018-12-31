60367cae2a6a45a0fd7aa76cd1a3f9f05554ec03
Abdelrahman Omran <me@omranic.com>
2018-12-31 15:16:12 +0200

    Seed member & manager accounts

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Cortex\Auth\Console\Commands;
 
 use Cortex\Auth\Models\Admin;
+use Cortex\Auth\Models\Member;
+use Cortex\Auth\Models\Manager;
 use Illuminate\Console\Command;
 use Cortex\Auth\Models\Guardian;
 
@@ -38,6 +40,8 @@ class SeedCommand extends Command
         // Create models
         $admin = $this->createAdmin($adminPassword = str_random());
         $guardian = $this->createGuardian($guardianPassword = str_random());
+        $manager = $this->createManager($managerPassword = str_random());
+        $member = $this->createMember($memberPassword = str_random());
 
         // Assign roles
         $admin->assign('superadmin');
@@ -45,6 +49,8 @@ class SeedCommand extends Command
         $this->table(['Username', 'Password'], [
             ['username' => $admin['username'], 'password' => $adminPassword],
             ['username' => $guardian['username'], 'password' => $guardianPassword],
+            ['username' => $manager['username'], 'password' => $managerPassword],
+            ['username' => $member['username'], 'password' => $memberPassword],
         ]);
     }
 
@@ -91,6 +97,58 @@ class SeedCommand extends Command
 
         return tap(app('cortex.auth.guardian')->firstOrNew($guardian)->fill([
             'remember_token' => str_random(10),
+            'password' => $password,
+        ]), function ($instance) {
+            $instance->save();
+        });
+    }
+
+    /**
+     * Create manager model.
+     *
+     * @param string $password
+     *
+     * @return \Cortex\Auth\Models\Manager
+     */
+    protected function createManager(string $password): Manager
+    {
+        $manager = [
+            'is_active' => true,
+            'username' => 'Manager',
+            'given_name' => 'Manager',
+            'family_name' => 'User',
+            'email' => 'manager@example.com',
+        ];
+
+        return tap(app('cortex.auth.manager')->firstOrNew($manager)->fill([
+            'remember_token' => str_random(10),
+            'email_verified_at' => now(),
+            'password' => $password,
+        ]), function ($instance) {
+            $instance->save();
+        });
+    }
+
+    /**
+     * Create member model.
+     *
+     * @param string $password
+     *
+     * @return \Cortex\Auth\Models\Member
+     */
+    protected function createMember(string $password): Member
+    {
+        $member = [
+            'is_active' => true,
+            'username' => 'Member',
+            'given_name' => 'Member',
+            'family_name' => 'User',
+            'email' => 'member@example.com',
+        ];
+
+        return tap(app('cortex.auth.member')->firstOrNew($member)->fill([
+            'remember_token' => str_random(10),
+            'email_verified_at' => now(),
             'password' => $password,
         ]), function ($instance) {
             $instance->save();
