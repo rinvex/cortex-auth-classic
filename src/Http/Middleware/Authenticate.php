2eadccc63fd1ff24088046ac14e8711c095f152b
Abdelrahman Omran <me@omranic.com>
2019-09-03 05:03:52 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -11,7 +11,8 @@ class Authenticate extends BaseAuthenticate
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return string
      */
     protected function redirectTo($request)
