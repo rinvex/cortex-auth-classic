ab2369d2e2404158f96c5c393c7ae153ea578c94
Abdelrahman Omran <me@omranic.com>
2020-01-08 02:00:45 +0400

    Enforce consistency

--- src/Http/Controllers/Frontarea/AuthenticationController.php
+++ src/Http/Controllers/Frontarea/AuthenticationController.php
@@ -90,9 +90,9 @@ class AuthenticationController extends AbstractController
      */
     protected function sendLoginResponse(Request $request)
     {
-        $user = auth()->guard($this->getGuard())->user();
+        $currentUser = $request->user($this->getGuard());
 
-        $twofactor = $user->getTwoFactor();
+        $twofactor = $currentUser->getTwoFactor();
         $totpStatus = $twofactor['totp']['enabled'] ?? false;
         $phoneStatus = $twofactor['phone']['enabled'] ?? false;
 
@@ -102,7 +102,7 @@ class AuthenticationController extends AbstractController
         if ($totpStatus || $phoneStatus) {
             $this->processLogout($request);
 
-            $request->session()->put('cortex.auth.twofactor', ['user_id' => $user->getKey(), 'remember' => $request->filled('remember'), 'totp' => $totpStatus, 'phone' => $phoneStatus]);
+            $request->session()->put('cortex.auth.twofactor', ['user_id' => $currentUser->getKey(), 'remember' => $request->filled('remember'), 'totp' => $totpStatus, 'phone' => $phoneStatus]);
 
             $route = $totpStatus
                 ? route('frontarea.verification.phone.verify')
