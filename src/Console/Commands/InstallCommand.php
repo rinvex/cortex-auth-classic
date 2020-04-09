02e4bf34f89d0fac66ea87d36b0f7c8396d13784
Abdelrahman Omran <me@omranic.com>
2020-04-09 10:31:26 +0400

    Change default: Publish config files only when installing

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -31,7 +31,7 @@ class InstallCommand extends Command
     {
         $this->alert($this->description);
 
-        $this->call('cortex:publish:auth', ['--force' => $this->option('force')]);
+        $this->call('cortex:publish:auth', ['--force' => $this->option('force'), '--resource' => 'config']);
         $this->call('cortex:migrate:auth', ['--force' => $this->option('force')]);
         $this->call('cortex:seed:auth');
     }
