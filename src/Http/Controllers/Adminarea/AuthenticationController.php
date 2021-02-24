a9781c0dcebec72734b8510b33756d647391d745
Abdelrahman Omran <me@omranic.com>
2021-02-24 04:07:39 +0400

    Utilize `UnauthenticatedController` for guest controllers

--- src/Http/Controllers/Adminarea/AuthenticationController.php
+++ src/Http/Controllers/Adminarea/AuthenticationController.php
@@ -6,10 +6,10 @@ namespace Cortex\Auth\Http\Controllers\Adminarea;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use Cortex\Foundation\Http\Controllers\AbstractController;
 use Cortex\Auth\Http\Requests\Adminarea\AuthenticationRequest;
+use Cortex\Foundation\Http\Controllers\UnauthenticatedController;
 
-class AuthenticationController extends AbstractController
+class AuthenticationController extends UnauthenticatedController
 {
     /**
      * {@inheritdoc}
@@ -17,16 +17,6 @@ class AuthenticationController extends AbstractController
     protected $middlewareWhitelist = [
         'logout',
     ];
-
-    /**
-     * Create a new authentication controller instance.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->middleware(($guard = request()->guard()) ? 'guest:'.$guard : 'guest')->except($this->middlewareWhitelist);
-    }
 
     /**
      * Show the login form.
