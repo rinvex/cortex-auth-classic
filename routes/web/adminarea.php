fa36e16abd54cb471c27c3c7327aa21de7907733
Abdelrahman Omran <me@omranic.com>
2021-08-15 18:16:23 +0400

    Register routes to either central or tenant domains

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-Route::domain(domain())->group(function () {
+Route::domain('{central_domain}')->group(function () {
     Route::name('adminarea.')
          ->middleware(['web', 'nohttpcache'])
          ->namespace('Cortex\Auth\Http\Controllers\Adminarea')
