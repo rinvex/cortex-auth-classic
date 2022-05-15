d352595cb84151f7ed3652ec3d33275737d46ad8
Abdelrahman Omran <me@omranic.com>
2022-05-15 16:51:09 +0300

    Fix correct naming for daterangepicker from datepicker

--- resources/views/adminarea/pages/member.blade.php
+++ resources/views/adminarea/pages/member.blade.php
@@ -212,7 +212,7 @@
                                     {{-- Birthday --}}
                                     <div class="form-group has-feedback{{ $errors->has('birthday') ? ' has-error' : '' }}">
                                         {{ Form::label('birthday', trans('cortex/auth::common.birthday'), ['class' => 'control-label']) }}
-                                        {{ Form::date('birthday', null, ['class' => 'form-control datepicker', 'data-locale' => '{"format": "YYYY-MM-DD"}', 'data-single-date-picker' => 'true', 'data-show-dropdowns' => 'true', 'data-auto-apply' => 'true', 'data-min-date' => '1900-01-01']) }}
+                                        {{ Form::date('birthday', null, ['class' => 'form-control daterangepicker', 'data-locale' => '{"format": "YYYY-MM-DD"}', 'data-single-date-picker' => 'true', 'data-show-dropdowns' => 'true', 'data-auto-apply' => 'true', 'data-min-date' => '1900-01-01']) }}
                                         <span class="fa fa-calendar form-control-feedback"></span>
 
                                         @if ($errors->has('birthday'))
