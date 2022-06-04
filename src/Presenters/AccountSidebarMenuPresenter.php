2842d2bc0bd984da5c517ded6a196bf6ada58b85
Abdelrahman Omran <me@omranic.com>
2022-06-05 02:02:31 +0400

    Add support for list item attributes

--- src/Presenters/AccountSidebarMenuPresenter.php
+++ src/Presenters/AccountSidebarMenuPresenter.php
@@ -55,8 +55,8 @@ class AccountSidebarMenuPresenter extends BasePresenter
      */
     public function getMenuWithoutDropdownWrapper(MenuItem $item): string
     {
-        return '<li class="'.($item->isActive() ? 'active' : '').'">
-                    <a href="'.$item->getUrl().'" '.$item->getAttributes().'>
+        return '<li '.$item->getItemAttributes().'>
+                    <a href="'.$item->getUrl().'" '.$item->getLinkAttributes().'>
                         '.($item->icon ? '<i class="'.$item->icon.'"></i>' : '').'
                         '.$item->title.'
                     </a>
