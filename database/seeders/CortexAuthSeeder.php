48173196f7b089eb06cf6f049b20724fa03e939d
Abdelrahman Omran <me@omranic.com>
2023-08-17 13:17:33 +0000

    Seed the new 'view' ability

--- database/seeders/CortexAuthSeeder.php
+++ database/seeders/CortexAuthSeeder.php
@@ -20,6 +20,7 @@ class CortexAuthSeeder extends Seeder
 
         $abilities = [
             ['name' => 'list', 'title' => 'List abilities', 'entity_type' => 'ability'],
+            ['name' => 'view', 'title' => 'View abilities', 'entity_type' => 'ability'],
             ['name' => 'import', 'title' => 'Import abilities', 'entity_type' => 'ability'],
             ['name' => 'export', 'title' => 'Export abilities', 'entity_type' => 'ability'],
             ['name' => 'create', 'title' => 'Create abilities', 'entity_type' => 'ability'],
@@ -29,6 +30,7 @@ class CortexAuthSeeder extends Seeder
             ['name' => 'grant', 'title' => 'Grant abilities', 'entity_type' => 'ability'],
 
             ['name' => 'list', 'title' => 'List roles', 'entity_type' => 'role'],
+            ['name' => 'view', 'title' => 'View roles', 'entity_type' => 'role'],
             ['name' => 'import', 'title' => 'Import roles', 'entity_type' => 'role'],
             ['name' => 'export', 'title' => 'Export roles', 'entity_type' => 'role'],
             ['name' => 'create', 'title' => 'Create roles', 'entity_type' => 'role'],
@@ -38,6 +40,7 @@ class CortexAuthSeeder extends Seeder
             ['name' => 'assign', 'title' => 'Assign roles', 'entity_type' => 'role'],
 
             ['name' => 'list', 'title' => 'List admins', 'entity_type' => 'admin'],
+            ['name' => 'view', 'title' => 'View admins', 'entity_type' => 'admin'],
             ['name' => 'import', 'title' => 'Import admins', 'entity_type' => 'admin'],
             ['name' => 'export', 'title' => 'Export admins', 'entity_type' => 'admin'],
             ['name' => 'create', 'title' => 'Create admins', 'entity_type' => 'admin'],
@@ -46,6 +49,7 @@ class CortexAuthSeeder extends Seeder
             ['name' => 'audit', 'title' => 'Audit admins', 'entity_type' => 'admin'],
 
             ['name' => 'list', 'title' => 'List members', 'entity_type' => 'member'],
+            ['name' => 'view', 'title' => 'View members', 'entity_type' => 'member'],
             ['name' => 'import', 'title' => 'Import members', 'entity_type' => 'member'],
             ['name' => 'export', 'title' => 'Export members', 'entity_type' => 'member'],
             ['name' => 'create', 'title' => 'Create members', 'entity_type' => 'member'],
@@ -54,6 +58,7 @@ class CortexAuthSeeder extends Seeder
             ['name' => 'audit', 'title' => 'Audit members', 'entity_type' => 'member'],
 
             ['name' => 'list', 'title' => 'List guardians', 'entity_type' => 'guardian'],
+            ['name' => 'view', 'title' => 'View guardians', 'entity_type' => 'guardian'],
             ['name' => 'import', 'title' => 'Import guardians', 'entity_type' => 'guardian'],
             ['name' => 'export', 'title' => 'Export guardians', 'entity_type' => 'guardian'],
             ['name' => 'create', 'title' => 'Create guardians', 'entity_type' => 'guardian'],
